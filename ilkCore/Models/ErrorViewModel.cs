3f5816889949666bf09172d02e368894e6dc1d81
ismail Çadır <ismailcadir@gmail.com>
2019-03-07 10:54:35 +0300

    Düzeltildi

---
+++ ilkCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ilkCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
