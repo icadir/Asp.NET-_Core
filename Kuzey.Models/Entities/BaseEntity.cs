5506b39c5a59d479d91c6283a69af9af08eda639
ismail Çadır <ismailcadir@gmail.com>
2019-03-18 09:51:32 +0300

    yapı ok

---
+++ Kuzey.Models/Entities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Kuzey.Models.Entities
+{
+    public class BaseEntity<T>
+    {
+        [Key]
+        public T Id { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        [StringLength(450)]
+        public string CreatedUserId { get; set; }
+    }
+}
